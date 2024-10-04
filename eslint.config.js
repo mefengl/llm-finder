677d01543e54bf8e4602ffb85ba4ecc39121d8ae
不游 <71683364+mefengl@users.noreply.github.com>
2024-10-04 13:52:34 +0800

    update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,37 @@
 import antfu from '@antfu/eslint-config'
 import { FlatCompat } from '@eslint/eslintrc'
-import perfectionistNatural from 'eslint-plugin-perfectionist/configs/recommended-natural'
+import perfectionist from 'eslint-plugin-perfectionist'
 
 const compat = new FlatCompat()
 
-export default antfu({
-  rules: {
-    'import/order': 'off',
-    'tailwindcss/migration-from-tailwind-2': 'off',
-    'tailwindcss/no-custom-classname': 'off',
+export default antfu(
+  {
+    rules: {
+      'tailwindcss/migration-from-tailwind-2': 'off',
+      'tailwindcss/no-custom-classname': 'off',
+    },
   },
-}, ...compat.config({
-  extends: [
-    'plugin:tailwindcss/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: [
-    'src/components/ui/*',
-    'src/lib/utils.ts',
-    'tailwind.config.js',
-  ],
-}), perfectionistNatural)
+  ...compat.config({
+    extends: [
+      'plugin:tailwindcss/recommended',
+      'plugin:react-hooks/recommended',
+    ],
+  }),
+
+  // perfectionist
+  {
+    rules: {
+      'import/order': 'off',
+      ...perfectionist.configs['recommended-natural'].rules,
+    },
+  },
+
+  // shadcn/ui
+  ...compat.config({
+    ignorePatterns: [
+      'src/components/ui/*',
+      'src/lib/utils.ts',
+      'tailwind.config.js',
+    ],
+  }),
+)
